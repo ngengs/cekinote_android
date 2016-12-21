fe894bfa3f3fe3a962d49c6b5a4fe0330ddb6cbd
rizky Kharisma <ngeng.ngengs@gmail.com>
2016-12-21 12:14:22 +0700

    Change game list item layout
    Optimize with remove empty view, just use the parent layout of image
    indicator instead empty view
    
    Signed-off-by: rizky Kharisma <ngeng.ngengs@gmail.com>

--- app/src/main/java/com/ngengs/android/cekinote/adapter/GameListAdapter.java
+++ app/src/main/java/com/ngengs/android/cekinote/adapter/GameListAdapter.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -160,7 +161,7 @@ public class GameListAdapter extends RecyclerView.Adapter<GameListAdapter.ViewHo
         @BindView(R.id.game_card)
         CardView gameCard;
         @BindView(R.id.indicator_selected_game)
-        RelativeLayout indicatorSelectedGame;
+        ImageView indicatorSelectedGame;
 
         ViewHolder(View view) {
             super(view);
